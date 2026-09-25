cb2a1b6fcde30d23788e20ee51ada138b3b57864
Fitra Ramadan <fcarier2006@gmail.com>
2026-09-25 19:17:43 +0700

    Add files via upload

---
+++ components/FooterComponent.jsx
@@ -0,0 +1,9 @@
+function FooterComponent() {
+  return (
+    <footer className="footer">
+      <p>© {new Date().getFullYear()} Fitra Ramadan.</p>
+    </footer>
+  )
+}
+
+export default FooterComponent
---
+++ components/HeaderComponent.jsx
@@ -0,0 +1,10 @@
+function HeaderComponent() {
+  return (
+    <header className="header">
+      <h1>About Me</h1>
+      <p className="tagline">Biodata</p>
+    </header>
+  )
+}
+
+export default HeaderComponent
