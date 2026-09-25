b43f6dc81e07a85fe7410271b22edc1e298392b8
Fitra Ramadan <fcarier2006@gmail.com>
2026-09-25 19:25:41 +0700

    Delete components directory

--- components/FooterComponent.jsx
+++
@@ -1,9 +0,0 @@
-function FooterComponent() {
-  return (
-    <footer className="footer">
-      <p>© {new Date().getFullYear()} Fitra Ramadan.</p>
-    </footer>
-  )
-}
-
-export default FooterComponent
