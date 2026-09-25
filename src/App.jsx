d65c02f7fede90611e65ef60b8aec520bd9e832e
Fitra Ramadan <fcarier2006@gmail.com>
2026-09-25 19:33:51 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import HeaderComponent from './components/HeaderComponent'
+import SectionComponent from './components/SectionComponent'
+import FooterComponent from './components/FooterComponent'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <HeaderComponent />
+      <SectionComponent />
+      <FooterComponent />
+    </div>
+  )
+}
+
+export default App
